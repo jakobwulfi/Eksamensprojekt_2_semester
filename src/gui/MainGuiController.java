d3d4a4f3ea197c10ceaf8f556a4a71348bba4607
Jaqass <jacobsorensen524@gmail.com>
2023-11-29 10:32:55 +0100

    Gui fxml style

---
+++ src/gui/MainGuiController.java
@@ -0,0 +1,282 @@
+package gui;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+public class MainGuiController {
+    @FXML
+    private Button btnOpretDestillat;
+
+    @FXML
+    private Button btnOpretFad;
+
+    @FXML
+    private Button btnOpretLager;
+
+    @FXML
+    private Button btnOpretPåfyldning;
+
+    @FXML
+    private Button btnTilføjFadTilLager;
+
+    @FXML
+    private Button btnTilføjWhisky;
+
+    @FXML
+    private Label lblAdresse;
+
+    @FXML
+    private Label lblAlkoholProcent;
+
+    @FXML
+    private Label lblAntalLiter;
+
+    @FXML
+    private Label lblDestillatPå;
+
+    @FXML
+    private Label lblDestillater;
+
+    @FXML
+    private Label lblDestillaterWhisky;
+
+    @FXML
+    private Label lblFadInfo;
+
+    @FXML
+    private Label lblFadNr;
+
+    @FXML
+    private Label lblFadOprindelse;
+
+    @FXML
+    private Label lblFadPå;
+
+    @FXML
+    private Label lblFadType;
+
+    @FXML
+    private Label lblFade;
+
+    @FXML
+    private Label lblFadeWhisky;
+
+    @FXML
+    private Label lblHylder;
+
+    @FXML
+    private Label lblInfoDestillat;
+
+    @FXML
+    private Label lblInfoFad;
+
+    @FXML
+    private Label lblKapacitet;
+
+    @FXML
+    private Label lblKommentar;
+
+    @FXML
+    private Label lblKornsort;
+
+    @FXML
+    private Label lblLager;
+
+    @FXML
+    private Label lblLagre;
+
+    @FXML
+    private Label lblMaltBatch;
+
+    @FXML
+    private Label lblMedarbejder;
+
+    @FXML
+    private Label lblNavn;
+
+    @FXML
+    private Label lblNewMakeNr;
+
+    @FXML
+    private Label lblPåfyldninger;
+
+    @FXML
+    private Label lblRygeMateriale;
+
+    @FXML
+    private Label lblRækker;
+
+    @FXML
+    private Label lblSlutDato;
+
+    @FXML
+    private Label lblStartDato;
+
+    @FXML
+    private Label lblStartVolume;
+
+    @FXML
+    private Label lblWhiskyer;
+
+    @FXML
+    private ComboBox<?> lstLager;
+
+    @FXML
+    private ListView<?> lvwDestillatPå;
+
+    @FXML
+    private ListView<?> lvwDestillater;
+
+    @FXML
+    private ListView<?> lvwDestillaterWhisky;
+
+    @FXML
+    private ListView<?> lvwFadPå;
+
+    @FXML
+    private ListView<?> lvwFade;
+
+    @FXML
+    private ListView<?> lvwFadeWhisky;
+
+    @FXML
+    private ListView<?> lvwLagre;
+
+    @FXML
+    private ListView<?> lvwPåfyldninger;
+
+    @FXML
+    private ListView<?> lvwWhiskyer;
+
+    @FXML
+    private AnchorPane pnDestillater;
+
+    @FXML
+    private AnchorPane pnFade;
+
+    @FXML
+    private AnchorPane pnLager;
+
+    @FXML
+    private AnchorPane pnPåfyldning;
+
+    @FXML
+    private AnchorPane pnWhisky;
+
+    @FXML
+    private Tab tabDestilleringer;
+
+    @FXML
+    private Tab tabFade;
+
+    @FXML
+    private Tab tabLager;
+
+    @FXML
+    private Tab tabPåfyldning;
+
+    @FXML
+    private Tab tabWhisky;
+
+    @FXML
+    private TextArea txaFadInfo;
+
+    @FXML
+    private TextArea txaInfoDestillat;
+
+    @FXML
+    private TextArea txaInfoFad;
+
+    @FXML
+    private TextField txfAdresse;
+
+    @FXML
+    private TextField txfAlkoholProcent;
+
+    @FXML
+    private TextField txfAntalLiter;
+
+    @FXML
+    private TextField txfFadNr;
+
+    @FXML
+    private TextField txfFadOprindelse;
+
+    @FXML
+    private TextField txfFadType;
+
+    @FXML
+    private TextField txfHylder;
+
+    @FXML
+    private TextField txfKapacitet;
+
+    @FXML
+    private TextField txfKommentar;
+
+    @FXML
+    private TextField txfKornsort;
+
+    @FXML
+    private TextField txfMaltBatch;
+
+    @FXML
+    private TextField txfMedarbejder;
+
+    @FXML
+    private TextField txfNavn;
+
+    @FXML
+    private TextField txfNewMakeNr;
+
+    @FXML
+    private TextField txfRygeMateriale;
+
+    @FXML
+    private TextField txfRækker;
+
+    @FXML
+    private TextField txfSlutDato;
+
+    @FXML
+    private TextField txfStartDato;
+
+    @FXML
+    private TextField txfStartVolume;
+
+    @FXML
+    void opretDestillatAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void opretFadAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void opretLagerAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void opretPåfyldningAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tilføjFadTilLagerAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tilføjWhiskyAction(ActionEvent event) {
+
+    }
+}
