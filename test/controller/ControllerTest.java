b9f8a3b326911969b2c28e964f4bea0da71ab9b7
Jakob Wolf <jakob-wolf14@live.dk>
2023-12-04 16:53:12 +0100

    Test af beregnprocent

--- test/controller/ControllerTest.java
+++ test/controller/ControllerTest.java
@@ -1,7 +1,9 @@
 package controller;
 
 import model.Destillat;
+import model.DestillatTilPåfyldning;
 import model.Fad;
+import model.Påfyldning;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import storage.ListStorage;
@@ -43,5 +45,21 @@ class ControllerTest {
 
         //Assert
         assertEquals(resultat, Controller.getFade().size());
+    }
+    //-------------------------------------------------
+    @Test
+    void opretPåfyldning() {
+        Fad f = Controller.opretFad("Frankrig", "Sherry", 1, 100);
+        Destillat nm1 = Controller.opretDestillat("Blended", "Bygsort", "Lars", 40, null, "Nice", "nm1",
+                LocalDate.of(2023,12,1), LocalDate.of(2026,12,2), 10);
+        Destillat nm2 = Controller.opretDestillat("Blended", "Bygsort", "Lars", 60, null, "Nice", "nm2",
+                LocalDate.of(2023, 12, 1), LocalDate.of(2026, 12, 2), 15);
+        DestillatTilPåfyldning d1 = Controller.opretDestillatTilPåfyldning(nm1, 10);
+        DestillatTilPåfyldning d2 = Controller.opretDestillatTilPåfyldning(nm2, 15);
+
+        Påfyldning påfyldning = Controller.opretPåfyldning(List.of(d1, d2), f, LocalDate.of(2023, 12, 06), "Lars",
+                LocalDate.of(2026, 12, 04));
+
+        assertEquals(52, påfyldning.getAlkoholProcent());
     }
 }
