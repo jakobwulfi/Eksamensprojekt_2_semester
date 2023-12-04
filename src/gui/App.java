849951c86be808ba30227542913b5222d11cae57
Jaqass <jacobsorensen524@gmail.com>
2023-12-04 12:09:56 +0100

    update

--- src/gui/App.java
+++ src/gui/App.java
@@ -1,9 +1,22 @@
 package gui;
 
+import controller.Controller;
+import controller.Storage;
 import javafx.application.Application;
+import storage.ListStorage;
+
 
 public class App {
     public static void main(String[] args) {
+        Storage storage = ListStorage.loadStorage();
+        if (storage == null) {
+            storage = new ListStorage();
+            System.out.println("Empty ListStorage created");
+        }
+        Controller.setStorage(storage);
+
         Application.launch(Gui.class);
+        ListStorage.saveStorage(storage);
+
     }
 }
