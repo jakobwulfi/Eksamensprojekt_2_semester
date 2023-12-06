1b1f24df0a8a43bdd13b342673e310a43ae72fef
kappertherapper <kasperjonassen@yahoo.dk>
2023-12-06 09:39:44 +0100

    findLokationPåFad oprettet

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public abstract class Controller {
     private static Storage storage;
+
     public static void setStorage(Storage storage) {
         Controller.storage = storage;
     }
@@ -18,7 +19,7 @@ public abstract class Controller {
      * Opret et nyt Destillat
      * Pre: startDato > Slutdato,
      */
-    public static Destillat opretDestillat(String maltBatch, String kornsort, String medarbejder,  double alkoholProcent,
+    public static Destillat opretDestillat(String maltBatch, String kornsort, String medarbejder, double alkoholProcent,
                                            String rygeMateriale, String kommentar, String newMakeNr, LocalDate startDato, LocalDate slutDato, double startVolume) {
         Destillat d = new Destillat(maltBatch, kornsort, medarbejder, alkoholProcent, rygeMateriale, kommentar, newMakeNr,
                 startDato, slutDato, startVolume);
@@ -56,7 +57,7 @@ public abstract class Controller {
      * Pre:
      */
     public static Fad opretFad(String fadFra, String fadType, int fadNr, double fadStørrelse) {
-        Fad f = new Fad(fadFra, fadType,  fadNr, fadStørrelse);
+        Fad f = new Fad(fadFra, fadType, fadNr, fadStørrelse);
         storage.storeFad(f);
         return f;
     }
@@ -85,7 +86,7 @@ public abstract class Controller {
      */
     public static Lager opretLager(String adresse, String navn, int rækker, int hylder, int pladsHylde) {
         Lager l = new Lager(adresse, navn);
-        opretRække(l,rækker,hylder,pladsHylde);
+        opretRække(l, rækker, hylder, pladsHylde);
         storage.storeLager(l);
         return l;
     }
@@ -112,7 +113,7 @@ public abstract class Controller {
      */
     public static void opretHylde(int maxKapacitet, int antal, Række række) {
         for (int i = 0; i < antal; i++) {
-            Hylde h = new Hylde(i+1, maxKapacitet);
+            Hylde h = new Hylde(i + 1, maxKapacitet);
             række.addHylde(h);
         }
     }
@@ -127,15 +128,16 @@ public abstract class Controller {
      * Opret ny Række
      * Pre:
      */
-    public static List<Række> opretRække(Lager lager, int rækker, int hylder,int pladsHylde) {
+    public static List<Række> opretRække(Lager lager, int rækker, int hylder, int pladsHylde) {
         for (int i = 0; i < rækker; i++) {
-            Række r = new Række(i+1, lager);
-            opretHylde(pladsHylde,hylder,r);
+            Række r = new Række(i + 1, lager);
+            opretHylde(pladsHylde, hylder, r);
             lager.addRække(r);
         }
         return lager.getRækker();
     }
     //---------------------------------------------------------------------
+
     /**
      * Opret en nyt Whisky
      * Pre:
@@ -166,12 +168,12 @@ public abstract class Controller {
      * Pre:
      */
     public static Påfyldning opretPåfyldning(List<DestillatTilPåfyldning> destillater, Fad fad, LocalDate startDato,
-                                             String medarbejder,  LocalDate slutDato) {
+                                             String medarbejder, LocalDate slutDato) {
         double volumen = 0;
-        for (DestillatTilPåfyldning destillatTilPåfyldning : destillater){
-            volumen+= destillatTilPåfyldning.getMængdeLiter();
+        for (DestillatTilPåfyldning destillatTilPåfyldning : destillater) {
+            volumen += destillatTilPåfyldning.getMængdeLiter();
         }
-        if (volumen > fad.getFadStørrelse()){
+        if (volumen > fad.getFadStørrelse()) {
             throw new IllegalArgumentException("Påfyldning for stor til fad");
         } else {
             Påfyldning p = new Påfyldning(destillater, startDato, medarbejder, slutDato);
@@ -184,19 +186,42 @@ public abstract class Controller {
      * Opret er DestillatTilPåfyldning
      */
 
-    public static DestillatTilPåfyldning opretDestillatTilPåfyldning(Destillat destillat, double mængdeLiter){
+    public static DestillatTilPåfyldning opretDestillatTilPåfyldning(Destillat destillat, double mængdeLiter) {
         if (destillat.getMængdeLiter() - mængdeLiter < 0) {
-           throw new IllegalArgumentException("Mængden af væske til påfyldning er større end mængden der er tilbage af destillatet");
+            throw new IllegalArgumentException("Mængden af væske til påfyldning er større end mængden der er tilbage af destillatet");
         }
-        DestillatTilPåfyldning d = new DestillatTilPåfyldning(destillat, mængdeLiter,destillat.getAlkoholProcent());
+        DestillatTilPåfyldning d = new DestillatTilPåfyldning(destillat, mængdeLiter, destillat.getAlkoholProcent());
         destillat.setMængdeLiter(destillat.getMængdeLiter() - mængdeLiter);
         return d;
     }
 
+    //---------------------------------------------------------------------
+
     /**
-     * Update Påfyldning.
-     * Pre:
+     * Finder lokation på fad
+     * Pre: fad oprettet
      */
+    public static String findLokationPåFad(Fad fad) {
+        for (Lager lager : storage.getLagere()) {
+            for (Række række : lager.getRækker()) {
+                for (Hylde hylde : række.getHylder()) {
+                    if (hylde.getFade().contains(fad)) {
+                        return "Lager: " + lager.getNavn() +
+                                ", Række: " + række.getRækkeNr() +
+                                ", Hylde: " + hylde.getHyldeNr();
+                    }
+                }
+            }
+        }
+        return "Fad ikke fundet i Lageret.";
+    }
+
+        //---------------------------------------------------------------------
+
+        /**
+         * Update Påfyldning.
+         * Pre:
+         */
     /*public static void updatePåfyldning(Påfyldning påfyldning, Destillat destillat, Fad fad, LocalDate startDato,
                                         String medarbejder, double antalLiter, double alkoholProcent, LocalDate slutDato) {
         //påfyldning.setFad(fad);
@@ -208,5 +233,5 @@ public abstract class Controller {
     }*/
 
 
-    //---------------------------------------------------------------------
-}
+        //---------------------------------------------------------------------
+    }
