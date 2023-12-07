0ec3001b41b6d84d5eacb6b2b484c107cc9f8a05
Jakob Wolf <jakob-wolf14@live.dk>
2023-12-07 10:35:38 +0100

    initStorage

--- src/gui/App.java
+++ src/gui/App.java
@@ -3,7 +3,11 @@ package gui;
 import controller.Controller;
 import controller.Storage;
 import javafx.application.Application;
+import model.*;
 import storage.ListStorage;
+
+import java.time.LocalDate;
+import java.util.List;
 
 
 public class App {
@@ -15,8 +19,38 @@ public class App {
         }
         Controller.setStorage(storage);
 
+        if (Controller.getLagere().isEmpty()) {
+            initStorage();
+            System.out.println("Storage initialized.");
+        }
+
         Application.launch(Gui.class);
         ListStorage.saveStorage(storage);
 
+    }
+
+    public static void initStorage() {
+         // fade
+        Fad fad1 = Controller.opretFad("Frankrig", "Sherry", 3, 60);
+        Fad fad2 = Controller.opretFad("Frankrig", "Bourbon", 4, 90);
+        Fad fad3 = Controller.opretFad("Italien", "Rødvin", 5, 120);
+        // lager
+        Lager lager1 = Controller.opretLager("Mosevej 3", "Snævar's baggård", 5, 3, 5);
+        Lager lager2 = Controller.opretLager("Blomstervej 17", "Depot Lager", 7, 3, 7);
+        // tilføj fad til lager
+        Controller.addFadTilHylde(fad1, 1, 1, lager1);
+        Controller.addFadTilHylde(fad2, 1, 1, lager1);
+        Controller.addFadTilHylde(fad3, 1, 1, lager2);
+        // destillat
+        Destillat d1 = Controller.opretDestillat("Corny Black Roast", "Byg","Snævar", 63.3, "Egetræ", "God batch",
+                "NM-34A", LocalDate.of(2020,01,01), LocalDate.of(2020,01,04), 120);
+        Destillat d2 = Controller.opretDestillat("Blonde Peachy Almond", "Byg","Niels", 70.7, "Egetræ", "Stærk batch",
+                "NM-34B", LocalDate.of(2020,01,01), LocalDate.of(2020,01,04), 80);
+        Destillat d3 = Controller.opretDestillat("Roasted Brown Nuts", "Byg","Snævar", 55.4, "Egetræ", "OK batch",
+                "NM-56A", LocalDate.of(2023,11,25), LocalDate.of(2023,11,28), 150);
+        // destillat til påfyldning
+        DestillatTilPåfyldning dtp = Controller.opretDestillatTilPåfyldning(d1, 120);
+        // påfyldning
+        Påfyldning p = Controller.opretPåfyldning(List.of(dtp), fad3, LocalDate.of(2020,01,01), "Snævar");
     }
 }
