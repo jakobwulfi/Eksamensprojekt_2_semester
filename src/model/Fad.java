c5d8f66f0b8f57b58989b2feeb5e02d154151405
kappertherapper <kasperjonassen@yahoo.dk>
2023-12-11 10:41:18 +0100

    FadInfo fjernet igen

--- src/model/Fad.java
+++ src/model/Fad.java
@@ -22,8 +22,6 @@ public class Fad implements Serializable{
         this.fadNr = fadNr;
         this.fadStørrelse = fadStørrelse;
     }
-    public Fad() {
-    }
 
     //---------------------------------------------------------------------
 
