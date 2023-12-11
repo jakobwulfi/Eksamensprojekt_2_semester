dd26e962813665bf9ae48bf8451f4a670d01a544
Jakob Wolf <jakob-wolf14@live.dk>
2023-12-11 14:27:34 +0100

    destillat mængde fix

--- src/gui/MainGuiController.java
+++ src/gui/MainGuiController.java
@@ -14,6 +14,7 @@ import model.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MainGuiController {
